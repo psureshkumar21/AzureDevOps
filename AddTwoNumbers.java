a7c8f6a10fe3ee364fb37987efdaac5dffa979de
onlnestudies <allonlinestudies@gmail.com>
2025-02-08 23:55:24 +0530

    AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class AddTwoNumbers
+{
+	public static void main(String args[])
+	{
+		Scanner obj = new Scanner(System.in);
+		int sum, num1, num2;
+		System.out.println("Enter First Number");
+		num1=obj.nextInt();
+		System.out.println("Enter Second Number:");
+		num2=obj.nextInt();
+		sum=num1+num2;
+		System.out.println("Sum of two numbers is":+sum);
+	}
+}
